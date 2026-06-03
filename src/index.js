c3bb009cab63656ec92cef4cb5813a69f408890a
Tanu Sable <tanusable@Tanus-MacBook-Air.local>
2026-06-03 16:24:35 +0530

    feat: DB connection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
-const http = require("http");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ message: "Backend running 🚀" }));
-});
+// require('dotenv').config({path: '.env'})
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+dotenv.config({
+  path: './.env'
+})
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constant";
+// import express from "express";
+// const app = express();
+// ;(async() => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("ERROR :", error);
+//       throw error;
+//     });
+//     app.listen(process.env.PORT, () => {
+//       console.log(`Server is running on port ${process.env.PORT}`);
+//     });
+//   }catch (error) {
+//     console.error("ERROR :", error);
+//     throw error;
+//   }
+// }) ()
